9a95aac3c797801bb388dc330ee432e48cadbb3a
RobbyWS <robbyww2@gmail.com>
2025-11-24 14:50:29 +0700

    awal commit

---
+++ home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h2>PROJECT IIR</h2>
+<form action="" method="POST">
+    <label>Nama Penulis:</label>
+    <input type="text" id="penulis" name="penulis"><br><br>
+    <label>Keyword Artikel:</label>
+    <input type="text" id="keyword" name="keyword"><br><br>
+    <label>Jumlah Data:</label>
+    <input type="text" id="jumlahData" name="jumlahData"><br><br>
+    <input type="submit" name="search" value="Search">
+</form>
+</body>
+</html>
